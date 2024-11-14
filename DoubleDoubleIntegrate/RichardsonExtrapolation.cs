35cdc71b0d2b2ff33b9a79aab20bda848bea8af2
tk-yoshimura <yoshimura8192@gmail.com>
2024-11-14 19:34:28 +0900

    fix: thread safe

--- DoubleDoubleIntegrate/RichardsonExtrapolation.cs
+++ DoubleDoubleIntegrate/RichardsonExtrapolation.cs
@@ -9,30 +9,38 @@ namespace DoubleDoubleIntegrate {
         readonly List<ddouble[]> values = new();
 
         public void Inject(ddouble new_value) {
-            if (SeriesCount <= 0) {
-                values.Add(new ddouble[] { new_value });
-                return;
+            lock (values) {
+                if (SeriesCount <= 0) {
+                    values.Add([new_value]);
+                    return;
+                }
+
+                ddouble[] t = values[SeriesCount - 1], t_next = new ddouble[SeriesCount + 1];
+
+                t_next[0] = new_value;
+
+                for (int i = 1; i <= SeriesCount; i++) {
+                    t_next[i] = t_next[i - 1] + (t_next[i - 1] - t[i - 1]) * R(i);
+                }
+
+                values.Add(t_next);
             }
-
-            ddouble[] t = values[SeriesCount - 1], t_next = new ddouble[SeriesCount + 1];
-
-            t_next[0] = new_value;
-
-            for (int i = 1; i <= SeriesCount; i++) {
-                t_next[i] = t_next[i - 1] + (t_next[i - 1] - t[i - 1]) * R(i);
-            }
-
-            values.Add(t_next);
         }
 
         private static ddouble R(int i) {
-            for (int k = rs.Count; k <= i; k++) {
-                ddouble r = 1d / (ddouble.Ldexp(1d, k * 2) - 1);
-
-                rs.Add(r);
+            if (i < rs.Count) {
+                return rs[i];
             }
 
-            return rs[i];
+            lock (rs) {
+                for (int k = rs.Count; k <= i; k++) {
+                    ddouble r = 1d / (ddouble.Ldexp(1d, k * 2) - 1);
+
+                    rs.Add(r);
+                }
+
+                return rs[i];
+            }
         }
 
         public IEnumerable<ddouble> Series {
